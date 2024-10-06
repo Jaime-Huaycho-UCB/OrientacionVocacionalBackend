de09c510dd71da0d92bfb3b6973ac7f9f8939e02
Jaime-Macbook <huaychojaime@gmail.com>
2024-10-05 22:19:40 -0400

    Creacion de proyecto

---
+++ app/Http/Middleware/Cors.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Http\Middleware;
+use Closure;
+class Cors{
+    public function handle($request, Closure $next)
+    {
+        $response = $next($request);
+        $response->headers->set('Access-Control-Allow-Origin', '*');
+        $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        $response->headers->set('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization');
+
+        // Opcional: responder directamente a solicitudes OPTIONS
+        if ($request->isMethod('OPTIONS')) {
+            return response('', 200)
+                ->header('Access-Control-Allow-Origin', '*')
+                ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+                ->header('Access-Control-Allow-Headers', 'Origin, Content-Type, Authorization');
+        }
+
+        return $response;
+    }
+}
